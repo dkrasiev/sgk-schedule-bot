afa286804070ba6d64d1bdf3334b1d8d11e313ef
Krasiev Dmitry <dmitrykrasiev@gmail.com>
2022-10-28 17:23:51 +0400

    update my context

--- src/types/context.type.ts
+++ src/types/context.type.ts
@@ -1,11 +1,13 @@
 import {Context} from 'telegraf';
 import I18n from 'telegraf-i18n';
+import {ChatDocument} from '../models/chat.model';
 import {GroupDocument} from '../models/group.model';
 
 export interface MyContext extends Context {
   i18n: I18n;
-  session?: {
+  session: {
     group?: GroupDocument;
+    chat?: ChatDocument;
     messageHasGroup?: boolean;
   };
 }
