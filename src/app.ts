e60c3f1dcf47862ba6d42ffa706ce04291acdb14
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2022-11-20 16:02:00 +0400

    use express and shelljs for running git pull on push to remote

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 import { run } from "@grammyjs/runner";
 import cron from "node-cron";
+import shjs from "shelljs";
+import express from "express";
 
 import { checkSchedule } from "./utils/bot-helpers";
 import bot from "./bot";
@@ -17,6 +19,20 @@ async function start() {
   if (runner.isRunning()) {
     logger.info("Bot is running");
   }
+
+  const app = express();
+  const port = process.env.PORT ?? 3000;
+
+  app.get("/pull", (req, res) => {
+    shjs.exec("git pull");
+    console.log(req);
+
+    res.send("Hello World!");
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 
   cron.schedule("*/15 * * * *", () => {
     checkSchedule(bot);
