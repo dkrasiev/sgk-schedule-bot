ac4adf08d32884a15bc47810ecb9b4d3eba8c905
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-03-31 17:37:22 +0400

    add session logger

--- src/composers/log.composer.ts
+++ src/composers/log.composer.ts
@@ -6,6 +6,7 @@ const logComposer = new Composer<MyContext>();
 
 logComposer.on("message:text", async (ctx, next) => {
   logger.info(ctx.message.text);
+  logger.info(JSON.stringify(ctx.session, undefined, 2));
 
   await next();
 });
