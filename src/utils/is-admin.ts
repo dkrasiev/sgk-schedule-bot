2405550384071eaf3f73f409e7abf965d7ae63e0
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2023-05-06 12:01:45 +0400

    refactor is admin

--- src/utils/is-admin.ts
+++ src/utils/is-admin.ts
@@ -1,7 +1,5 @@
 import { ADMINS } from "../config";
 
 export function isAdmin(username?: string): boolean {
-  if (!username) return false;
-
-  return ADMINS.includes(username);
+  return username ? ADMINS.includes(username) : false;
 }
